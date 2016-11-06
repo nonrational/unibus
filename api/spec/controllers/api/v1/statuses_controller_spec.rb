bb39cbb6687f442c2414ce4a407a861ef71e1a3d
Alan Norton <alan@mealpal.com>
2016-11-06 16:26:30 -0500

    wow.

--- api/spec/controllers/api/v1/statuses_controller_spec.rb
+++ api/spec/controllers/api/v1/statuses_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::StatusesController do
+  routes { Api::Engine.routes }
+
   it 'responds to GET' do
-    get :show, format: :json
+    get :show
     expect(JSON.parse(response.body)).to eq('status' => 'ok')
   end
 end
